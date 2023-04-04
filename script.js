ca825b8b7e8d635c4aeb9649d83e98d85b382f65
Akshay Nagar <118043847+akshaynagar14@users.noreply.github.com>
2023-04-04 15:41:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-let rows=document.querySelector("#output");
+function generateRandomTime() {
+  return Math.floor(Math.random() * 3) + 1; // returns a random number between 1 and 3
+}
 
-	rows.innerHTML=
-		`<tr id="loading">
-		  <td colspan=2 >Loading...</td>
-		  </tr>`;
+function createPromise() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(generateRandomTime());
+    }, generateRandomTime() * 1000);
+  });
+}
 
-let p1 = new Promise((resolve,reject)=>{
-	let time1=Math.random()*2+1;
-	setTimeout(()=>{
-		resolve(time1);
-	},time1*1000);
+const promises = [createPromise(), createPromise(), createPromise()];
+
+Promise.all(promises).then(results => {
+  const table = document.getElementById('myTable');
+  table.innerHTML = ''; // clear the loading text
+
+  // add the rows to the table
+  const row1 = table.insertRow();
+  const cell11 = row1.insertCell();
+  const cell12 = row1.insertCell();
+  cell11.innerText = 'Promise 1';
+  cell12.innerText = `${results[0]}`;
+
+  const row2 = table.insertRow();
+  const cell21 = row2.insertCell();
+  const cell22 = row2.insertCell();
+  cell21.innerText = 'Promise 2';
+  cell22.innerText = `${results[1]}`;
+
+  const row3 = table.insertRow();
+  const cell31 = row3.insertCell();
+  const cell32 = row3.insertCell();
+  cell31.innerText = 'Promise 3';
+  cell32.innerText = `${results[2]}`;
+
+  const row4 = table.insertRow();
+  const cell41 = row4.insertCell();
+  const cell42 = row4.insertCell();
+  cell41.innerText = 'Total';
+  cell42.innerText = `${(results[0] + results[1] + results[2]).toFixed(3)}`;
 });
 
-let p2 = new Promise((resolve,reject)=>{
-	let time2=Math.random()*2+1;
-	setTimeout(()=>{
-		resolve(time2);
-	},time2*1000);
-});
-
-let p3 = new Promise((resolve,reject)=>{
-	let time3=Math.random()*2+1;
-	setTimeout(()=>{
-		resolve(time3);
-	},time3*1000);
-});
-
-Promise.all([p1,p2,p3]).then((result)=>{
-	let sum=0;
-	rows.innerHTML="";
-	for(var i=0;i<result.length;i++){
-		sum +=result[i];
-		rows.innerHTML +=
-			`<tr>
-			   <td>Promise ${i+1}</td>
-				<td>${result[i]}</td>
-			</tr>`;
-		
-	}
-	rows.innerHTML +=
-			`<tr>
-			   <td>Total</td>
-				<td>${sum}</td>
-			</tr>`;
 })
